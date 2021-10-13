1334a7ea03cf309545a9e51b191495e61ffe5e75
dingyx99 <yuxiaoding@acm.org>
2021-10-13 23:49:23 +0800

    Rewrite parameter processing policy

--- login.py
+++ login.py
@@ -1,29 +1,33 @@
 import sys, getopt
 from nwafu_login import NwafuLogin
 
-def main():
+def guide(argv):
+    print("Usage:\n"+argv+" -u <username> -p <password> \n"+argv+" --username=<username> --password=<password>\n")
+
+def main(argv):
     username = ''
     passwd = ''
-    argv = sys.argv[1:]
-    if len(argv) != 4:
-        print("Usage:\npython3 login.py -u <username> -p <password> \npython3 login.py --username=<username> --password=<password>\n")
+    try:
+        opts, args = getopt.getopt(argv, "hu:p:", ["username=", "password="])
+    except getopt.GetoptError as err:
+        guide(sys.argv[0])
         sys.exit(2)
-    else:
-        try:
-            opts, args = getopt.getopt(argv, "hu:p:", ["username=", "password="])
-        except getopt.GetoptError:
-            print("Usage:\npython3 login.py -u <username> -p <password> \npython3 login.py --username=<username> --password=<password>\n")
-            sys.exit(2)
-        for opt, arg in opts:
-            if opt == '-h':
-                print("Usage:\npython3 login.py -u <username> -p <password> \npython3 login.py --username=<username> --password=<password>\n")
-                sys.exit()
-            elif opt in ['-u', '--username']:
-                username = arg
-            elif opt in ['-p', '--password']:
-                passwd = arg
+    username = None
+    passwd = None
+    for opt, arg in opts:
+        if opt == '-h':
+            guide(sys.argv[0])
+            sys.exit()
+        elif opt in ['-u', '--username']:
+            username = arg
+        elif opt in ['-p', '--password']:
+            passwd = arg
+    if username == None or passwd == None:
+        guide(sys.argv[0])
+        sys.exit(2)
     
     NwafuLogin(username=username, password=passwd).log_out()
     NwafuLogin(username=username, password=passwd).log_in()
 
-main()
+if __name__ ==  "__main__":
+    main(sys.argv[1:])
--- main.py
+++ main.py
@@ -1,27 +1,30 @@
 import sys, getopt
 from nwafu_login import NwafuLogin
 
-def main():
+def guide(argv):
+    print("Usage:\n"+argv+" -u <username> -p <password> \n"+argv+" --username=<username> --password=<password>\n")
+
+def main(argv):
     username = ''
     passwd = ''
-    argv = sys.argv[1:]
-    if len(argv) != 4:
-        print("Usage:\npython3 main.py -u <username> -p <password> \npython3 main.py --username=<username> --password=<password>\n")
+    try:
+        opts, args = getopt.getopt(argv, "hu:p:", ["username=", "password="])
+    except getopt.GetoptError as err:
+        guide(sys.argv[0])
         sys.exit(2)
-    else:
-        try:
-            opts, args = getopt.getopt(argv, "hu:p:", ["username=", "password="])
-        except getopt.GetoptError:
-            print("Usage:\npython3 main.py -u <username> -p <password> \npython3 main.py --username=<username> --password=<password>\n")
-            sys.exit(2)
-        for opt, arg in opts:
-            if opt == '-h':
-                print("Usage:\npython3 main.py -u <username> -p <password> \npython3 main.py --username=<username> --password=<password>\n")
-                sys.exit()
-            elif opt in ['-u', '--username']:
-                username = arg
-            elif opt in ['-p', '--password']:
-                passwd = arg
+    username = None
+    passwd = None
+    for opt, arg in opts:
+        if opt == '-h':
+            guide(sys.argv[0])
+            sys.exit()
+        elif opt in ['-u', '--username']:
+            username = arg
+        elif opt in ['-p', '--password']:
+            passwd = arg
+    if username == None or passwd == None:
+        guide(sys.argv[0])
+        sys.exit(2)
 
     a = NwafuLogin(username=username, password=passwd)
     while 1:
@@ -44,4 +47,5 @@ def main():
         else:
             print('\n{: ^28}'.format('Input error!'))
 
-main()
+if __name__ ==  "__main__":
+    main(sys.argv[1:])
